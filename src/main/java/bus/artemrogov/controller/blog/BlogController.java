f8d25bc65e63fdb9ded2ac5dcd72a21cd531ddad
Рогов Артем Владимирович <rogar@gkomega.ru>
2022-05-09 21:59:40 +0300

    dto object

--- src/main/java/bus/artemrogov/controller/blog/BlogController.java
+++ src/main/java/bus/artemrogov/controller/blog/BlogController.java
@@ -1,9 +1,10 @@
 package bus.artemrogov.controller.blog;
 
-
 import bus.artemrogov.entity.Comment;
 import bus.artemrogov.entity.Post;
+import bus.artemrogov.response.PostDtoResponse;
 import bus.artemrogov.service.IBlogService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,14 +22,22 @@ public class BlogController {
     private IBlogService blogService;
 
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+
     @GetMapping(value = "/")
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody Page<Post> posts(
+    public @ResponseBody Page<PostDtoResponse> posts(
             @RequestParam(value = "limit",  defaultValue  =  "10")  Integer   limit,
             @RequestParam(value = "page",   defaultValue  =  "0")   Integer   page
     ){
         Pageable pageable = PageRequest.of(page,limit, Sort.Direction.DESC,"id");
-        return this.blogService.getPostsList(pageable);
+
+        Page<Post> posts = this.blogService.getPostsList(pageable);
+
+        return posts.map(this::convertToDtoPostDto);
     }
 
 
@@ -42,5 +51,10 @@ public class BlogController {
     ){
         Pageable pageable = PageRequest.of(page,limit, Sort.Direction.DESC,"id");
         return this.blogService.getCommentsByPostId(postId,pageable);
+    }
+
+
+    private PostDtoResponse convertToDtoPostDto(Post entityPost) {
+        return modelMapper.map(entityPost, PostDtoResponse.class);
     }
 }
